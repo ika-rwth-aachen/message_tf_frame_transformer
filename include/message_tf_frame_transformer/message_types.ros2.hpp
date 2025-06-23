134312f5ff722422cd4410a2860d86f4e92e2435
Lukas Zanger <lukas.zanger@ika.rwth-aachen.de>
2025-06-23 10:17:10 +0200

    Add perception_interfaces and planning_interfaces

--- include/message_tf_frame_transformer/message_types.ros2.hpp
+++ include/message_tf_frame_transformer/message_types.ros2.hpp
@@ -9,5 +9,21 @@
 #include <geometry_msgs/msg/vector3_stamped.hpp>
 #include <geometry_msgs/msg/wrench_stamped.hpp>
 
+#include <tf2_perception_msgs/tf2_perception_msgs.hpp>
+#include <perception_msgs/msg/ego_data.hpp>
+#include <perception_msgs/msg/object.hpp>
+#include <perception_msgs/msg/object_list.hpp>
+#include <perception_msgs/msg/object_state.hpp>
+
+#include <tf2_route_planning_msgs/tf2_route_planning_msgs.hpp>
+#include <route_planning_msgs/msg/lane_boundary.hpp>
+#include <route_planning_msgs/msg/lane_element.hpp>
+#include <route_planning_msgs/msg/regulatory_element.hpp>
+#include <route_planning_msgs/msg/route.hpp>
+#include <route_planning_msgs/msg/route_element.hpp>
+
 #include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+
+#include <tf2_trajectory_planning_msgs/tf2_trajectory_planning_msgs.hpp>
+#include <trajectory_planning_msgs/msg/trajectory.hpp>
