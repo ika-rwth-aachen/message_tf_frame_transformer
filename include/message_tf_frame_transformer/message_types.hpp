e707f76cd51b36b4c1dd12b118913f572eac11a8
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2025-08-18 11:51:32 +0200

    comment unsued geometry_msgs headers

--- include/message_tf_frame_transformer/message_types.hpp
+++ include/message_tf_frame_transformer/message_types.hpp
@@ -1,22 +1,22 @@
 #pragma once
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <geometry_msgs/msg/accel.hpp>
-#include <geometry_msgs/msg/accel_stamped.hpp>
-#include <geometry_msgs/msg/accel_with_covariance.hpp>
-#include <geometry_msgs/msg/accel_with_covariance_stamped.hpp>
-#include <geometry_msgs/msg/inertia.hpp>
-#include <geometry_msgs/msg/inertia_stamped.hpp>
+// #include <geometry_msgs/msg/accel.hpp>
+// #include <geometry_msgs/msg/accel_stamped.hpp>
+// #include <geometry_msgs/msg/accel_with_covariance.hpp>
+// #include <geometry_msgs/msg/accel_with_covariance_stamped.hpp>
+// #include <geometry_msgs/msg/inertia.hpp>
+// #include <geometry_msgs/msg/inertia_stamped.hpp>
 #include <geometry_msgs/msg/point.hpp>
 #include <geometry_msgs/msg/point32.hpp>
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <geometry_msgs/msg/polygon.hpp>
-#include <geometry_msgs/msg/polygon_instance.hpp>
-#include <geometry_msgs/msg/polygon_instance_stamped.hpp>
+// #include <geometry_msgs/msg/polygon_instance.hpp>
+// #include <geometry_msgs/msg/polygon_instance_stamped.hpp>
 #include <geometry_msgs/msg/polygon_stamped.hpp>
 #include <geometry_msgs/msg/pose.hpp>
-#include <geometry_msgs/msg/pose2_d.hpp>
-#include <geometry_msgs/msg/pose_array.hpp>
+// #include <geometry_msgs/msg/pose2_d.hpp>
+// #include <geometry_msgs/msg/pose_array.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/pose_with_covariance.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
@@ -24,13 +24,13 @@
 #include <geometry_msgs/msg/quaternion_stamped.hpp>
 #include <geometry_msgs/msg/transform.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
-#include <geometry_msgs/msg/twist_with_covariance.hpp>
-#include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
+// #include <geometry_msgs/msg/twist.hpp>
+// #include <geometry_msgs/msg/twist_stamped.hpp>
+// #include <geometry_msgs/msg/twist_with_covariance.hpp>
+// #include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
 #include <geometry_msgs/msg/vector3.hpp>
 #include <geometry_msgs/msg/vector3_stamped.hpp>
-#include <geometry_msgs/msg/velocity_stamped.hpp>
+// #include <geometry_msgs/msg/velocity_stamped.hpp>
 #include <geometry_msgs/msg/wrench.hpp>
 #include <geometry_msgs/msg/wrench_stamped.hpp>
 
